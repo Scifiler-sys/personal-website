640cf41d2682c1fbcc7b10ab99e7565f42d5c2db
Scifiler-sys <pagdilaostephen@gmail.com>
2022-06-28 15:32:39 -0400

    Added simple navbar

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {  RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
-const routes: Routes = [
-  {path:"", component:AppComponent}
-];
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +12,9 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  exports: [RouterModule]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
