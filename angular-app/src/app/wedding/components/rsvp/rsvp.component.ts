0c274482a0340eab47b017e1419ae49b538ba652
Scifiler-sys <pagdilaostephen@gmail.com>
2022-07-01 18:59:18 -0400

    Added rsvp structure

--- angular-app/src/app/wedding/components/rsvp/rsvp.component.ts
+++ angular-app/src/app/wedding/components/rsvp/rsvp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rsvp',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rsvp.component.css']
 })
 export class RSVPComponent implements OnInit {
+
+  private sub?: Subscription;
+  private weddingDate:Date = new Date("Dec 17 2022 00:00:00");
+
+  // public day
+
+  private calculateTimeDiff() {
+    let currentDate = new Date();
+    // this.timeDifference = currentDate.getTime() - this.weddingDate.getTime();
+
+
+  }
 
   constructor() { }
 
