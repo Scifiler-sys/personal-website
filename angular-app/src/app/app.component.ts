7a6c9169ffb75e8d7f0e23bb3aebb431957c47f5
Scifiler-sys <pagdilaostephen@gmail.com>
2022-07-06 01:42:07 -0400

    Final update, added heading for better scrolling

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = "Stephen and Sigrid's Wedding";
 }
