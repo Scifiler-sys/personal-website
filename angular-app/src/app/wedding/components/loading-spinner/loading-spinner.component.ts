642bf0085176db87319047fa8bd4deda4dac5c28
Scifiler-sys <pagdilaostephen@gmail.com>
2022-07-06 03:03:05 -0400

    Added percentages

--- angular-app/src/app/wedding/components/loading-spinner/loading-spinner.component.ts
+++ angular-app/src/app/wedding/components/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent implements OnInit {
+
+  private _total:number = 0;
+  @Input('total')
+  set total(total:number){
+    this._total = total;
+    
+    //Hardcoded to only work with 6 elements
+    if (total !=0) {
+      this.percent = Math.round(this._total/6*100); //Change this line to make it more dynamic
+    }
+    else{
+      this.percent = 0;
+    }
+  }
+  
+  percent:number = 0;
 
   constructor() { }
 
--- angular-app/src/app/wedding/wedding.component.ts
+++ angular-app/src/app/wedding/wedding.component.ts
@@ -27,7 +27,7 @@ export class WeddingComponent implements OnInit {
       this.isFixed = true;
       //Will calculate what image to show after a certain px is hit
       this.currentImage = Math.min(Math.max(1, Math.floor(scrollHeight / 100) + 1),6);
-      console.log(this.currentImage);
+      // console.log(this.currentImage);
     }
   }
 
