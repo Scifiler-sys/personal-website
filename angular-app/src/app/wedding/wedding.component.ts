4052d42c3b4d45f3489f2223d8053a3e198178d6
Scifiler-sys <pagdilaostephen@gmail.com>
2022-07-06 01:10:35 -0400

    Fixed loader

--- angular-app/src/app/wedding/wedding.component.ts
+++ angular-app/src/app/wedding/wedding.component.ts
@@ -1,47 +1,51 @@
-import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wedding',
   templateUrl: './wedding.component.html',
   styleUrls: ['./wedding.component.css']
 })
-export class WeddingComponent implements OnInit, AfterViewInit {
+export class WeddingComponent implements OnInit {
 
   currentImage = "../../assets/Wedding/001.JPG";
   // Will add in 500px div after scrolling is done
-  isFixed:boolean = true;
+  isFixed: boolean = true;
   // Will show loader until loaded
-  isLoaded:boolean = true;
+  isLoaded: boolean = true;
+  //Counts how many images are loaded
+  imageTotal: number = 0;
 
+  //Listens to scroll event and change image based on scroll
+  @HostListener("window:scroll", ["$event"])
+  onScroll($event: any): void {
+    // console.log($event);
+    let scrollHeight = $event.srcElement.scrollingElement.scrollTop;
 
-  @HostListener("window:scroll", ["$event"]) 
-    onScroll($event: any): void {
-      // console.log($event);
-      let scrollHeight = $event.srcElement.scrollingElement.scrollTop
-      console.log(scrollHeight);
-
-      if (scrollHeight > 500) {
-        this.isFixed = false;
-      }
-      else
-      {
-        this.isFixed = true;
-        //Will calculate what image to show after a certain px is hit
-        let currentPhotoNumber: number = Math.max(1,Math.round(scrollHeight / 100)+1);
-        console.log(currentPhotoNumber)
-        this.currentImage = `../../assets/Wedding/00${currentPhotoNumber}.JPG`
-      }
+    if (scrollHeight > 500) {
+      this.isFixed = false;
     }
+    else {
+      this.isFixed = true;
+      //Will calculate what image to show after a certain px is hit
+      let currentPhotoNumber: number = Math.max(1, Math.round(scrollHeight / 100) + 1);
+      this.currentImage = `../../assets/Wedding/00${currentPhotoNumber}.JPG`
+    }
+  }
+
+  imageLoaded(){
+    this.imageTotal +=1;
+    console.log(this.imageTotal);
+
+    if (this.imageTotal == 6) {
+      this.isLoaded = false;
+    }
+  }
 
   constructor() {
     // Resets view back to the very top
     window.scroll({
-      top:0
+      top: 0
     })
-  }
-
-  ngAfterViewInit(): void {
-    this.isLoaded = false;
   }
 
   ngOnInit(): void {
